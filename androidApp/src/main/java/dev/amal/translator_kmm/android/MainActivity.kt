71d1d6146d78b0f8cd14a2414a15e3310ba29784
JustAmal <amalkrasavchik@gmail.com>
2022-12-26 14:33:29 +0600

    Implementing translate history and finishing Android translate screen

--- androidApp/src/main/java/dev/amal/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/dev/amal/translator_kmm/android/MainActivity.kt
@@ -7,19 +7,23 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import dev.amal.translator_kmm.android.core.presentation.Routes
 import dev.amal.translator_kmm.android.translate.presentation.AndroidTranslateViewModel
 import dev.amal.translator_kmm.android.translate.presentation.TranslateScreen
+import dev.amal.translator_kmm.translate.presentation.TranslateEvent
 
 @ExperimentalAnimationApi
 @ExperimentalComposeUiApi
@@ -52,7 +56,26 @@ fun TranslateRoot() {
         composable(route = Routes.TRANSLATE) {
             val viewModel = hiltViewModel<AndroidTranslateViewModel>()
             val state by viewModel.state.collectAsState()
-            TranslateScreen(state = state, onEvent = viewModel::onEvent)
+            TranslateScreen(
+                state = state,
+                onEvent = { event ->
+                    when (event) {
+                        is TranslateEvent.RecordAudio -> navController.navigate(
+                            Routes.VOICE_TO_TEXT + "/${state.fromLanguage.language.langCode}"
+                        )
+                        else -> viewModel.onEvent(event)
+                    }
+                }
+            )
+        }
+        composable(
+            route = Routes.VOICE_TO_TEXT + "/{languageCode}",
+            arguments = listOf(navArgument("languageCode") {
+                type = NavType.StringType
+                defaultValue = "en"
+            })
+        ) {
+            Text(text = "Voice-to-Text")
         }
     }
 }
--- androidApp/src/main/java/dev/amal/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/dev/amal/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -5,21 +5,24 @@ import android.widget.Toast
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import dev.amal.translator_kmm.android.R
-import dev.amal.translator_kmm.android.translate.presentation.components.LanguageDropDown
-import dev.amal.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
-import dev.amal.translator_kmm.android.translate.presentation.components.TranslateTextField
-import dev.amal.translator_kmm.android.translate.presentation.components.rememberTextToSpeech
+import dev.amal.translator_kmm.android.translate.presentation.components.*
+import dev.amal.translator_kmm.translate.domain.translate.TranslateError
 import dev.amal.translator_kmm.translate.presentation.TranslateEvent
 import dev.amal.translator_kmm.translate.presentation.TranslateState
 import java.util.*
@@ -36,10 +39,37 @@ fun TranslateScreen(
     val clipboardManager = LocalClipboardManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
 
+    LaunchedEffect(key1 = state.error) {
+        val message = when(state.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> context.getString(R.string.error_service_unavailable)
+            TranslateError.CLIENT_ERROR -> context.getString(R.string.client_error)
+            TranslateError.SERVER_ERROR -> context.getString(R.string.server_error)
+            TranslateError.UNKNOWN_ERROR -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(TranslateEvent.OnErrorSeen)
+        }
+    }
+
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = {
+                    onEvent(TranslateEvent.RecordAudio)
+                },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(75.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.mic),
+                    contentDescription = stringResource(id = R.string.record_audio)
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { padding ->
         LazyColumn(
             modifier = Modifier
@@ -112,6 +142,20 @@ fun TranslateScreen(
                     onTextFieldClick = {
                         onEvent(TranslateEvent.EditTranslation)
                     }
+                )
+            }
+            item {
+                if (state.history.isNotEmpty()) {
+                    Text(
+                        text = stringResource(id = R.string.history),
+                        style = MaterialTheme.typography.h2
+                    )
+                }
+            }
+            items(state.history) { item ->
+                TranslateHistoryItem(
+                    item = item,
+                    onClick = { onEvent(TranslateEvent.SelectHistoryItem(item)) }
                 )
             }
         }
